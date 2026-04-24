03d08c83ed4c09b39925ad648aa8f73c5ba5d294
qjy02 <jyquek32@gmail.com>
2026-04-24 22:42:18 +0800

    Update: SSR Changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-01-19',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image'
+  ],
   
   // GitHub Pages
   app: {
     baseURL: process.env.NODE_ENV === 'production' ? '/vivianquekjiayi/' : '/',
-    buildAssetsDir: '_nuxt/', // Important for assets
-    cdnURL: 'https://qjy02.github.io/vivianquekjiayi/', // Full CDN URL
+    buildAssetsDir: '_nuxt/',
+    cdnURL: 'https://qjy02.github.io/vivianquekjiayi/',
   },
   
   // Enable static generation
-  ssr: false, // Use false for GitHub Pages (static site)
+  ssr: true,
   
   // Route rules
   routeRules: {
